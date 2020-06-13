da10000fd33c75ff2653b2ba568446a32e0fcc8a
NicoAlvarez <nicolas.alv3@gmail.com>
2020-06-12 21:53:08 -0300

    Material and navbar set

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+class NavBar extends React.Component{
+    render(){
+        return(
+            <div>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="title" color="inherit">
+                            DistribuidoraJD
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
