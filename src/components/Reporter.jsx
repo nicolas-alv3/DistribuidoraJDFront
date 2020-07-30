96cbe9815943fd9ce246c7d0c75b90227f00372d
NicoAlvarez <nicolas.alv3@gmail.com>
2020-07-30 15:12:48 -0300

    Reporte finalizado

--- src/components/Reporter.jsx
+++ src/components/Reporter.jsx
@@ -6,34 +6,17 @@ class Report extends React.Component {
     jsreport.serverUrl = 'http://localhost:5488';
     const reportRequest = {
       template: {
-        shortid: 'rkJTnK2ce',
+        shortid: 'SklM5ro2lP',
       },
-      data: {
-        number: '123',
-        seller: {
-          name: 'Next Step Webs, Inc.',
-          road: '12345 Sunny Road',
-          country: 'Sunnyville, TX 12345',
-        },
-        buyer: {
-          name: 'Acme Corp.',
-          road: '16 Johnson Road',
-          country: 'Paris, France 8060',
-        },
-        items: [{
-          name: 'Website design',
-          price: 300,
-        }],
-      },
+      data: { },
     };
-    jsreport.render(this.reportPreview, reportRequest);
+    jsreport.render('_blank', reportRequest);
   }
 
   render() {
     return (
       <div
         style={{ height: '700px' }}
-        ref={(el) => (this.reportPreview = el)}
       />
     );
   }
